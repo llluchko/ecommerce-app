4d9aa3f529cc1bbe93bc4403c82dc7cc0affd45c
llluchko <l.mladenov21@gmail.com>
2021-12-28 17:00:07 +0200

    added SignInSignUp page, SignIn component, FormInput and Custom Button components

---
+++ src/components/form-input/form-input.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './form-input.styles.scss';
+
+// functional component - we don't need any state here (maybe just props??) - buble up any changes
+const FormInput = ({ handleChange, label, ...otherProps }) => (
+  <div className='group'>
+    <input className='form-input' onChange={handleChange} {...otherProps} />
+    {label ? (
+      <label
+        className={`${
+          otherProps.value.length ? 'shrink' : ''
+        } form-input-label`}
+      >
+        {label}
+      </label>
+    ) : null}
+  </div>
+);
+
+export default FormInput;
