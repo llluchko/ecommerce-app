efc7f7b44a61bbf629d5aa4887326c39102a92b7
llluchko <l.mladenov21@gmail.com>
2022-01-09 11:02:53 +0200

    updated shop data with new data; updated cart uitls to group items in cart reducer

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,23 +1,39 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+import { addItem } from '../../redux/cart/cart.actions';
+
 import CustomButton from '../custom-button/custom-button.component';
 
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ name, price, imageUrl }) => (
-  <div className='collection-item'>
-    <div
-      className='image'
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    />
-    <div className='collection-footer'>
-      <span className='name'> {name} </span>
-      <span className='price'> {price} </span>
+// if we use {}, we are rendering multiple javascript out of this function
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
+  return (
+    <div className='collection-item'>
+      <div
+        className='image'
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className='collection-footer'>
+        <span className='name'> {name} </span>
+        <span className='price'> {price} </span>
+      </div>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        Add to cart
+      </CustomButton>
     </div>
-    <CustomButton>Add to cart</CustomButton>
-  </div>
-);
+  );
+};
 
-export default CollectionItem;
+// when we dispatch / call this function
+// dispatch the object in to the store
+const mapDispatchToPros = (dispatch) => ({
+  // addItem is prop that return action
+  addItem: (item) => dispatch(addItem(item)),
+});
+
+export default connect(null, mapDispatchToPros)(CollectionItem);
