029a8ed2d08fb5bd56fe1bcf58842f62f2ea5a3f
llluchko <l.mladenov21@gmail.com>
2022-01-12 19:41:45 +0200

    created respective selectors for directory/shop

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,8 +1,36 @@
 import { createSelector } from 'reselect';
+import memoize from 'lodash.memoize';
+
+// store the data in object instead of an array is called data normalization
+// const COLLECTION_ID_MAP = {
+//   hats: 1,
+//   sneakers: 2,
+//   jackets: 3,
+//   womens: 4,
+//   mens: 5,
+// };
 
 const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector(
   [selectShop],
   (shop) => shop.collections
+);
+
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  (collections) =>
+    // return array with the keys from an object
+    Object.keys(collections).map((key) => collections[key])
+);
+
+// Memoize does the same idea of memoization as reselect does for our selectors, except this time we're memoizing the return of our function which returns our selector:
+export const selectCollection = memoize((collectionUrlParam) =>
+  createSelector(
+    [selectCollections],
+    (collections) => collections[collectionUrlParam]
+    // collections.find(
+    //   (collection) => collection.id == COLLECTION_ID_MAP[collectionUrlParam]
+    // )
+  )
 );
