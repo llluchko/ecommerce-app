ee821ead7e70ba273b031fc673f0028e12377004
llluchko <l.mladenov21@gmail.com>
2022-02-08 11:36:25 +0200

    headaer Contact link changed to home

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -26,7 +26,7 @@ const Header = ({ currentUser, hidden }) => (
     </LogoContainer>
     <OptionsContainer>
       <OptionLink to='/shop'>SHOP</OptionLink>
-      <OptionLink to='/shop'>CONTACT</OptionLink>
+      <OptionLink to='/'>CONTACT</OptionLink>
       {currentUser ? (
         <OptionLink as='div' onClick={() => auth.signOut()}>
           SIGN OUT
